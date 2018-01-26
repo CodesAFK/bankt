4e3fb74f1188f2e688e8b0ee2ef1f8ab6c159a36
CodesAFK <codesafk@gmail.com>
2018-01-26 17:54:51 -0600

    added admin deposits

---
+++ src/app/admin/admin-deposits/admin-deposits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-deposits',
+  templateUrl: './admin-deposits.component.html',
+  styleUrls: ['./admin-deposits.component.css']
+})
+export class AdminDepositsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DepositsComponent } from './deposits/deposits.component';
 import { TransactionSuccessComponent } from './transaction-success/transaction-success.component';
 import { PendingTransactionsComponent } from './pending-transactions/pending-transactions.component';
 import { AdminAccountsComponent } from './admin/admin-accounts/admin-accounts.component';
+import { AdminDepositsComponent } from './admin/admin-deposits/admin-deposits.component';
 
 
 @NgModule({
@@ -27,7 +28,8 @@ import { AdminAccountsComponent } from './admin/admin-accounts/admin-accounts.co
     DepositsComponent,
     TransactionSuccessComponent,
     PendingTransactionsComponent,
-    AdminAccountsComponent
+    AdminAccountsComponent,
+    AdminDepositsComponent
   ],
   imports: [
     BrowserModule,
